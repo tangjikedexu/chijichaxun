c3b607c9da0d42c490cfa484493aad578d4f6a0a
Your Name <you@example.com>
2019-01-18 10:05:09 +0800

    框架的构建

--- main.c
+++ main.c
@@ -1,9 +1,60 @@
 #include <stdio.h>
+struct student{
+    int yuwen;
+    int shuxue;
+    int yingyu;
+    char name[100]
+    
+};
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    printf("chengjichaxunxinftong \n");
+    while (1)
+    {
+        printf("1--- \n");
+        printf("2--- \n");
+        printf("3--- \n");
+
+        printf("4--- \n");
+        printf("5--- \n");
+        printf("6--- \n");
+        printf("7--- \n");
+        printf("8---tuichu \n");
+
+        printf("qingxuanze \n");
+
+        int code;
+        scanf("%d",&code);
+        
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+        }
+        if (code == 6)
+        {
+        }
+        if (code == 7)
+        {
+        }
+        if (code == 8)
+        {
+            printf("jieshu \n");
+            break;
+        }
+    }
 
     return 0;
-
 }
